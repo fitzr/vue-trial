5dc4f62b0a37802beb3278e3bcf620601a8dd6a6
fitzr <fitzroy.t.hagi@gmail.com>
2018-06-19 15:25:24 +0900

    use decorator

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 // @ts-ignore
-import App from './App.vue'
+import App from './views/App/index.vue'
 
 new Vue({
     render: h => h(App)
---
+++ src/views/App/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="red">{{msg}}</h1>
+</template>
+
+<script src="./script.ts" lang="ts"></script>
+
+<style>
+  .red {
+    color: #f00;
+  }
+</style>
