e337c8afbf2bec063c9d93be2dfed9d0ac2e963e
fitzr <fitzroy.t.hagi@gmail.com>
2018-07-01 22:12:40 +0900

    update

--- src/views/App23/Product/index.vue
+++ src/views/App23/Product/index.vue
@@ -13,10 +13,24 @@
     import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
     import { namespace } from 'vuex-class'
     import { Product as ProductType } from '@/api/products'
+    import {Route} from "vue-router";
 
     const product = namespace('product')
 
-    @Component
+    @Component({
+        beforeRouteEnter(to, from, next) {
+            console.log('component:beforeRouteEnter')
+            next()
+        },
+        beforeRouteUpdate(to, from, next) {
+            console.log('component:beforeRouteUpdate')
+            next()
+        },
+        beforeRouteLeave(to, from, next) {
+            console.log('component:beforeRouteLeave')
+            next((vm: Product) => {})
+        },
+    })
     export default class Product extends Vue {
         @Prop()
         id: number
@@ -37,6 +51,5 @@
         // async onIdChanged(newId: number) {
         //     this.item = await Database.asyncFind(newId)
         // }
-
     }
 </script>
